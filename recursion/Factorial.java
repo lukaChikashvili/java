8d29c5ab0b3cacce20490ae5228c846949d66219
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-12 20:18:52 +0400

    add sum of digits with recursion

--- recursion/Factorial.java
+++ recursion/Factorial.java
@@ -3,6 +3,7 @@ package recursion;
 public class Factorial {
     public static void main(String[] args) {
         System.out.println(factorial(5));
+        System.out.println(sum(1225));
     }
 
 
@@ -10,5 +11,14 @@ public class Factorial {
        if(n <= 1) return 1;
 
       return n * factorial(n - 1);
+    }
+
+// sum of digits
+    static int sum(int n) {
+       
+        if(n == 0) return 0;
+
+        return (n%10) + sum(n / 10);
+        
     }
 }

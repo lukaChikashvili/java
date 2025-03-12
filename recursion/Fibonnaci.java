2cc172c8fcb077487b233e1411fb67e78613231d
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-12 19:39:20 +0400

    add fibonacci numbers in java

---
+++ recursion/Fibonnaci.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class Fibonnaci {
+    public static void main(String[] args) {
+        System.out.println(fibo(25));
+
+    }
+
+    static int fibo(int n) {
+        if(n < 2) {
+            return n;
+        }
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}

f37f1588c1b12af6835e853182b2baf897867f34
luka chikashvili <lukachikashvili2020@gmail.com>
2025-03-17 20:36:35 +0400

    check if array is sorted with recursion

---
+++ recursion/IsSorted.java
@@ -0,0 +1,24 @@
+package recursion;
+
+public class IsSorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, 6, 8, 10};
+        System.out.println(sorted(arr, 0));
+    }
+
+    static boolean sorted(int[] arr, int index) {
+
+          if(index == arr.length - 1) {
+            return true;
+          }
+
+          if(arr[index] < arr[index + 1]) {
+            
+             return sorted(arr, index + 1);
+          }else {
+            return false;
+          }
+
+          
+    }
+}
